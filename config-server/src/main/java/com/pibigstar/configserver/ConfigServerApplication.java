8823871f9c69fe0033764f9c9587a68481a92359
pibigstar <leikewei123@163.com>
2018-11-20 19:07:56 +0800

    配置文件服务器注册到注册中心#6

--- config-server/src/main/java/com/pibigstar/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/pibigstar/configserver/ConfigServerApplication.java
@@ -2,10 +2,14 @@ package com.pibigstar.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer // 开启配置服务器功能
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
